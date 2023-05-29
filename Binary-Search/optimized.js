599a33df0f5be44fd0bb90434076f4d3de121422
woo kyung park <wpark1104@gmail.com>
2023-05-29 15:18:04 -0700

    Optimized

---
+++ Binary-Search/optimized.js
@@ -0,0 +1,17 @@
+function binarySeach (nums, target) {
+    let start =0;
+    let end = nums.length -1;
+
+    while (start < end) {
+        curr = Math.floor((end-start)/2) + start;
+        if (nums[curr] === target) {
+            return curr;
+        } else if (target < nums[curr]) {
+            end = curr -1;
+        } else {
+            start = curr + 1;
+        }
+    }
+    return -1;
+}
+console.log (binarySeach([-15, -9, -4, -1, 2, 5, 16, 30, 56], -9));
